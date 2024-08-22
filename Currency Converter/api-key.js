e68620336ed8c210d4a2b77900ccbf5a13cb50fe
Siddarth <siddarth3055@gmail.com>
2024-08-22 10:00:53 +0530

    Add files via upload

---
+++ Currency Converter/api-key.js
@@ -0,0 +1,2 @@
+//Paste your generated api Key here
+let apiKey = "583db9870f8ae84b9a6ae2d2";
---
+++ Currency Converter/script.js
@@ -0,0 +1,51 @@
+let api = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
+const fromDropDown = document.getElementById("from-currency-select");
+const toDropDown = document.getElementById("to-currency-select");
+
+//Create dropdown from the currencies array
+currencies.forEach((currency) => {
+  const option = document.createElement("option");
+  option.value = currency;
+  option.text = currency;
+  fromDropDown.add(option);
+});
+
+//Repeat same thing for the other dropdown
+currencies.forEach((currency) => {
+  const option = document.createElement("option");
+  option.value = currency;
+  option.text = currency;
+  toDropDown.add(option);
+});
+
+//Setting default values
+fromDropDown.value = "USD";
+toDropDown.value = "INR";
+
+let convertCurrency = () => {
+  //Create References
+  const amount = document.querySelector("#amount").value;
+  const fromCurrency = fromDropDown.value;
+  const toCurrency = toDropDown.value;
+
+  //If amount input field is not empty
+  if (amount.length != 0) {
+    fetch(api)
+      .then((resp) => resp.json())
+      .then((data) => {
+        let fromExchangeRate = data.conversion_rates[fromCurrency];
+        let toExchangeRate = data.conversion_rates[toCurrency];
+        const convertedAmount = (amount / fromExchangeRate) * toExchangeRate;
+        result.innerHTML = `${amount} ${fromCurrency} = ${convertedAmount.toFixed(
+          2
+        )} ${toCurrency}`;
+      });
+  } else {
+    alert("Please fill in the amount");
+  }
+};
+
+document
+  .querySelector("#convert-button")
+  .addEventListener("click", convertCurrency);
+window.addEventListener("load", convertCurrency);
